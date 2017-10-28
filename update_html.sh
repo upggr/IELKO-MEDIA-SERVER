92e19fe555850862aaefebb4fc622f37c862ebbe
Ioannis Kokkinis <info@upg.gr>
2017-10-29 00:17:30 +0300

    Updated favicon.ico.

--- update_html.sh
+++ update_html.sh
@@ -7,5 +7,6 @@ cp ~/working/IELKO/www/index.php /usr/local/nginx/html/index.php
 cp ~/working/IELKO/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-server.css
 cp ~/working/IELKO/www/stream.xml /usr/local/nginx/html/stream.xml
 cp ~/working/IELKO/www/testing.png /usr/local/nginx/html/testing.png
+cp ~/working/IELKO/www/favicon.ico /usr/local/nginx/html/favicon.ico
 git clone https://github.com/upggr/ielko-video-player /usr/local/nginx/html/player
 sudo rm -rf ~/working
