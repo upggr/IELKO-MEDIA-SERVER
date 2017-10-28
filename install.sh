4d6bbbbc7a6c5bd31b8e22751e07679f898b3459
Ioannis Kokkinis <info@upg.gr>
2017-10-28 23:56:45 +0300

    Updated testing.png.

--- install.sh
+++ install.sh
@@ -37,6 +37,7 @@ rm /usr/local/nginx/html/*
 cp ~/working/IELKO/www/index.php /usr/local/nginx/html/index.php
 cp ~/working/IELKO/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-server.css
 cp ~/working/IELKO/www/stream.xml /usr/local/nginx/html/stream.xml
+cp ~/working/IELKO/www/testing.png /usr/local/nginx/html/testing.png
 git clone https://github.com/upggr/ielko-video-player /usr/local/nginx/html/player
 #ip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//' )
 #sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/conf/nginx.conf
