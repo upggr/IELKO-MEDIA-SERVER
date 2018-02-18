1e47aa967b08682b4be4b4bae2c1335cdaa5cb90
Ioannis Kokkinis <info@upg.gr>
2018-02-19 01:03:02 +0200

    Updated update_html_custom_domain.sh.

---
+++ update_html_custom_domain.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir ~/working
+mkdir ~/working/IELKO
+git clone https://github.com/upggr/IELKO-MEDIA-SERVER.git ~/working/IELKO
+rm -rf /usr/local/nginx/html/*
+cp ~/working/IELKO/www/index.php /usr/local/nginx/html/index.php
+cp ~/working/IELKO/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-server.css
+cp ~/working/IELKO/www/stream.xml /usr/local/nginx/html/stream.xml
+cp ~/working/IELKO/www/testing.png /usr/local/nginx/html/testing.png
+cp ~/working/IELKO/www/favicon.ico /usr/local/nginx/html/favicon.ico
+git clone https://github.com/upggr/ielko-video-player /usr/local/nginx/html/player
+
+ip=$1
+sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/html/stream.xml
+sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/html/index.php
+#sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/conf/nginx.conf
+sudo rm -rf ~/working
